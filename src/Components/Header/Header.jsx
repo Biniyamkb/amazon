385b29016d47ba95d95beba7fe2e8986d4a94497
Biniyamkb <168299482+Biniyamkb@users.noreply.github.com>
2025-03-20 20:19:34 +0300

    Header stiky part

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
   const [{ basket }, dispatch] = useContext(DataContext);
 
   return (
-    <>
+    <section className={classes.fixed}>
       <section>
         <div className={classes.header_container}>
           <div className={classes.logo_container}>
@@ -72,7 +72,7 @@ const Header = () => {
         </div>
       </section>
       <LowerHeader />
-    </>
+    </section>
   );
 };
 
